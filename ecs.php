64843b563c0b19a1701ef3cef23fd263b1068b2f
grummbeer <kde@streber24.de>
2024-07-30 11:57:12 +0200

    test

---
+++ ecs.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/functions.php',
+    ])
+
+    // add a single rule
+    ->withRules([
+    ])
+
+    // add sets - group of rules
+    ->withPreparedSets(
+        psr12: true,
+         // arrays: true,
+         // namespaces: true,
+         spaces: true,
+         // docblocks: true,
+         // comments: true,
+     )
+
+     ;
---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ */
+function name (  $b= null , $c  = 'foo'  )
+{
+
+
+    return 'foo';
+
+
+}
+
+echo (name('b'));
