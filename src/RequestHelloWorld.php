a148c110d64c2d237c5e121449c76f85d3fda1ef
grummbeer <kde@streber24.de>
2024-08-04 03:36:15 +0200

    cs

--- src/RequestHelloWorld.php
+++ src/RequestHelloWorld.php
@@ -4,8 +4,7 @@ namespace CiTesting;
 
 class RequestHelloWorld
 {
-    public static function helloWorld(): ?string
-    {
+    public static function helloWorld(): ?string{
         return file_get_contents('http://localhost:4444/');
     }
 
